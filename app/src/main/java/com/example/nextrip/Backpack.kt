72bdd138d91917a7ec41d89eca963e84eb1b0436
cbjayanath <66734455+cbjayanath@users.noreply.github.com>
2022-12-15 19:51:31 +0530

    Added Backpack Activity

---
+++ app/src/main/java/com/example/nextrip/Backpack.kt
@@ -0,0 +1,11 @@
+package com.example.nextrip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Backpack : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_backpack)
+    }
+}
--- app/src/main/java/com/example/nextrip/Member.kt
+++ app/src/main/java/com/example/nextrip/Member.kt
@@ -1,8 +1,10 @@
 package com.example.nextrip
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -18,10 +20,11 @@ class Member : AppCompatActivity() {
     private lateinit var recv: RecyclerView
     private lateinit var memberList:ArrayList<MemberData>
     private lateinit var memberAdapter:MemberAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_member)
-
+        var nextbtn=findViewById<Button>(R.id.memberNextBtn)
         memberList = ArrayList()/**set List*/
 
         /**set find Id*/
@@ -37,6 +40,9 @@ class Member : AppCompatActivity() {
             addInfo()
         }
 
+        nextbtn.setOnClickListener{
+            startActivity(Intent(this,Backpack::class.java))
+        }
 
     }
 
